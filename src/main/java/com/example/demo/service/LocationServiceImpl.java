c081c0d445fd52115fa4dda58a93574f3b57eb7e
bala-amypo <bala@amypo.in>
2025-12-18 04:04:14 +0000

    User commit

--- src/main/java/com/example/demo/service/LocationServiceImpl.java
+++ src/main/java/com/example/demo/service/LocationServiceImpl.java
@@ -25,20 +25,3 @@ public class LocationServiceImpl implements LocationService {
     }
 }
 
-
-demo apllication
-
-
-package com.example.demo;
-
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-@SpringBootApplication
-public class DemoApplication {
-
-    public static void main(String[] args) {
-        SpringApplication.run(DemoApplication.class, args);
-    }
-
-}
